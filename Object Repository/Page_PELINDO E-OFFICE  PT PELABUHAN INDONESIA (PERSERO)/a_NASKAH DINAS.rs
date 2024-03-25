d05e366cee6b9845321f5d3065a74c73d3c57de6
Romy Kautsar <Romy Kautsar@10.1.9.21>
2024-03-25 10:44:20 +0700

    Add test case Log in PEO

---
+++ Object Repository/Page_PELINDO E-OFFICE  PT PELABUHAN INDONESIA (PERSERO)/a_NASKAH DINAS.rs
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_NASKAH DINAS</name>
+   <tag></tag>
+   <elementGuidId>95bf33e3-0a1c-442a-9cd7-a7e5c51f6a4f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'NASKAH DINAS')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>li.menu-dropdown.classic-menu-dropdown.open > a.hover-initialized</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>9d87cfb9-65c3-45ce-9333-69e119628fc5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-hover</name>
+      <type>Main</type>
+      <value>megamenu-dropdown</value>
+      <webElementGuid>abf6cf85-e692-440b-adb7-00bff04a2d03</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-close-others</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>c9e4ef6f-11de-4682-ada9-2c2551bc78f5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-toggle</name>
+      <type>Main</type>
+      <value>dropdown</value>
+      <webElementGuid>eeabad64-c8c5-4b0a-be02-b1e092b99eb7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>javascript:;</value>
+      <webElementGuid>0fdcf228-5734-4b5a-a921-ce295793eb08</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>hover-initialized</value>
+      <webElementGuid>fb776575-a5ca-4532-8656-212b50b9c9e7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-expanded</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>e574f477-11b5-4ed9-9ddf-ebfb377e0b93</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+						NASKAH DINAS 
+						</value>
+      <webElementGuid>9cb77f01-b1db-4cf3-bd15-4f2aa0f166c1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;page-md page-header-top-fixed bg-light-blue-c&quot;]/div[@class=&quot;page-header&quot;]/div[@class=&quot;page-header-menu&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;hor-menu&quot;]/ul[@class=&quot;nav navbar-nav&quot;]/li[@class=&quot;menu-dropdown classic-menu-dropdown open&quot;]/a[@class=&quot;hover-initialized&quot;]</value>
+      <webElementGuid>18d56ead-2c0b-4716-b95d-3f8d2bdc6c95</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'NASKAH DINAS')]</value>
+      <webElementGuid>fb608c6d-3f42-4570-91ea-64b217aff2c5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='UPLOAD LAMPIRAN'])[1]/preceding::a[1]</value>
+      <webElementGuid>6a2297d3-e09e-4e64-b354-2c8d9a3799cf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KOTAK MASUK'])[1]/preceding::a[2]</value>
+      <webElementGuid>e36d85ab-8333-4f8a-a67d-54ab32dcb82c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='NASKAH DINAS']/parent::*</value>
+      <webElementGuid>0251a940-0a37-499c-b3f0-634d31c74030</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, 'javascript:;')])[3]</value>
+      <webElementGuid>416758fd-6581-4532-9a0e-92a88459dca8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/a</value>
+      <webElementGuid>4a5f33db-392f-4125-b7b2-03a36d62da40</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'javascript:;' and (text() = '
+						NASKAH DINAS 
+						' or . = '
+						NASKAH DINAS 
+						')]</value>
+      <webElementGuid>99f88329-d216-45a5-9988-95b04a514bbb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
