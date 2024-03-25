6f41e7ddeaddd52253445bef05a758812b99f23a
Romy Kautsar <Romy Kautsar@10.1.9.21>
2024-03-25 09:07:53 +0700

    Add PEO dan Eproc landing Page

--- Test Suites/Aplikasi PELINDO.ts
+++ Test Suites/Aplikasi PELINDO.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>IBS</name>
+   <name>Aplikasi PELINDO</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
